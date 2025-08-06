ae1ce064b72672fe7f1c7574dfe526e2689eacb6
shivver83 <46364022+shivver83@users.noreply.github.com>
2025-08-06 20:46:42 +0530

    Update ContactPage.tsx

--- src/ContactPage.tsx
+++ src/ContactPage.tsx
@@ -3,9 +3,9 @@ function ContactPage() {
   return (
     <div style={{ padding: '2rem' }}>
       <h2>Contact Us</h2>
-      <p>Email: contact@happinesscreations.com</p>
-      <p>Phone: +91-9876543210</p>
-      <p>Address: 123 Numerology Lane, Harmony City, India</p>
+      <p>Email: nventer@gmail.com</p>
+      <p>Phone: +91-9212155970</p>
+      <p>Address: Sector 51, Noida near Metro station, Hoshiarpur </p>
     </div>
   );
 }
