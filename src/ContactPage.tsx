6278fee37b8d69ba2b1b117b8aa01e8094284606
shivver83 <46364022+shivver83@users.noreply.github.com>
2025-08-11 15:17:45 +0530

    Update ContactPage.tsx

--- src/ContactPage.tsx
+++ src/ContactPage.tsx
@@ -4,7 +4,7 @@ function ContactPage() {
     <div style={{ padding: '2rem' }}>
       <h2>Contact Us</h2>
       <p>Email: nventer@gmail.com</p>
-      <p>Phone: +91-9212155970</p>
+      <p>Phone: +91-7428552116</p>
       <p>Address: Sector 51, Noida near Metro station, Hoshiarpur </p>
     </div>
   );
